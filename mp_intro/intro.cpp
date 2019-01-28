39fd20d34e8d41ec65ae888ad81a49b21183bda3
Yutaro Ito <yutaroi2@siebl-0224-06.ews.illinois.edu>
2019-01-28 15:50:56 -0600

    file submission

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -2,15 +2,28 @@
 #include "cs225/HSLAPixel.h"
 
 #include <string>
+using namespace cs225;
 
 
 void rotate(std::string inputFile, std::string outputFile) {
   // TODO: Part 2
+  PNG* input = new PNG();
+  input->readFromFile(inputFile);
+  unsigned int width = input->width();
+  unsigned int height = input->height();
+  PNG* output = new PNG(width, height);
+
+  for (unsigned x = 0; x < width; x++) {
+    for (unsigned y = 0; y < height; y++) {
+      output->getPixel(x, y) = input->getPixel(width - 1 - x, height - 1 - y);
+    }
+  }
+  output->writeToFile(outputFile);
 }
 
-cs225::PNG myArt(unsigned int width, unsigned int height) {
-  cs225::PNG png(width, height);
+PNG myArt(unsigned int width, unsigned int height) {
+  PNG png(width, height);
   // TODO: Part 3
-
+  
   return png;
 }
