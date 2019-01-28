a6f288e22da2b3d7b68c236ac77950e14caed8cf
Yutaro Ito <yutaroi2@siebl-0224-06.ews.illinois.edu>
2019-01-28 16:16:22 -0600

    file submission

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -24,6 +24,15 @@ void rotate(std::string inputFile, std::string outputFile) {
 PNG myArt(unsigned int width, unsigned int height) {
   PNG png(width, height);
   // TODO: Part 3
-  
+  PNG* input = new PNG();
+  input->readFromFile("in_01.png");
+  for (unsigned x = 0; x < width; x++) {
+    for (unsigned y = 0; y < height; y++) {
+      png.getPixel(x, y).h = input->getPixel(x, y).h;
+      png.getPixel(x, y).s = input->getPixel(x, y).s;
+      png.getPixel(x, y).l = input->getPixel(x, y).l;
+      png.getPixel(x, y).a = input->getPixel(x, y).a;
+    }
+  }
   return png;
 }
