ded60cd67c68a2899ea92fe11f6c46e9df065433
Yutaro Ito <yutaroi2@siebl-0220-11.ews.illinois.edu>
2019-02-10 18:13:33 -0600

    file submission

--- mp_stickers/main.cpp
+++ mp_stickers/main.cpp
@@ -3,25 +3,26 @@
 
 int main() {
   Image base, png1, png2, png3, png4, png5, png6;
-  base.readFromFile("alma.png");
+  base.readFromFile("space.png");
   png1.readFromFile("pika.png");
   png2.readFromFile("mario.png");
   png3.readFromFile("doraemon.png");
   png4.readFromFile("yossy.png");
-  png5.readFromFile("splatoon.png");
-  png6.readFromFile("japan.png");
+  //png5.readFromFile("splatoon.png");
+  png6.readFromFile("kirby.png");
 
-  base.scale(2.0);
+  base.scale(1.6);
   png2.scale(0.4);
-  png5.scale(2.0);
+  //png5.scale(2.0);
+  png1.scale(0.6);
 
   StickerSheet test(base, 6);
-  test.addSticker(png1, 200, 200);
-  test.addSticker(png2, 1100, 200);
-  test.addSticker(png3, 1000, 0);
-  test.addSticker(png4, 100, 600);
-  test.addSticker(png5, 450, 300);
-  test.addSticker(png6, 800, 900);
+  test.addSticker(png1, 400, 600);
+  test.addSticker(png2, 1300, 200);
+  test.addSticker(png3, 1200, 0);
+  test.addSticker(png4, 2400, 1000);
+  //test.addSticker(png5, 600, 50);
+  test.addSticker(png6, 2200, 100);
   Image mine = test.render();
   //
   // Reminder:
