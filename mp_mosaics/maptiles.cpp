e79da81c07a5690191e298039fa1c100d8373be3
Yutaro Ito <yutaroi2@siebl-0222-07.ews.illinois.edu>
2019-03-31 16:56:37 -0500

    got started part2

--- mp_mosaics/maptiles.cpp
+++ mp_mosaics/maptiles.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <map>
 #include "maptiles.h"
+#include "point.h"
+#include "kdtree.h"
 //#include "cs225/RGB_HSL.h"
 
 using namespace std;
@@ -22,6 +24,28 @@ MosaicCanvas* mapTiles(SourceImage const& theSource,
      * @todo Implement this function!
      */
 
-    return NULL;
-}
+    int row = theSource.getRows();
+    int column = theSource.getColumns();
+    MosaicCanvas *canvas = new MosaicCanvas(row, column);
 
+    map<Point<3>, TileImage*> map;
+    vector<Point<3>> points;
+    for (TileImage tile : theTiles) {
+      Point<3> point = convertToXYZ(tile.getAverageColor());
+      points.push_back(point);
+      map[point] = &tile;
+    }
+
+    KDTree<3> tree(points);
+
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; i < column; j++) {
+        Point<3> regionColor = convertToXYZ(theSource.getRegionColor(i, j));
+        Point<3> nearest = tree.findNearestNeighbor(regionColor);
+
+        canvas->setTile(i, j, map[nearest]);
+      }
+    }
+
+    return canvas;
+}
