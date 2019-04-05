56b2831177462d72f0edd38333ed92e009558772
Yutaro Ito <yutaroi2@siebl-0224-03.ews.illinois.edu>
2019-04-05 17:52:34 -0500

    part1-a done

--- mp_mazes/dsets.cpp
+++ mp_mazes/dsets.cpp
@@ -1,1 +1,41 @@
 /* Your code here! */
+
+#include <iostream>
+#include "dsets.h"
+
+using std::cout;
+using std::endl;
+
+void DisjointSets::addelements(int num) {
+  for (int i = 0; i < num; i++) {
+    sets_.push_back(-1);
+  }
+}
+
+int DisjointSets::find(int elem) {
+  if (sets_[elem] < 0) {
+    return elem;
+  } else {
+    int root = find(sets_[elem]);
+    sets_[elem] = root;
+    return root;
+  }
+}
+
+void DisjointSets::setunion(int a, int b) {
+  int root1 = find(a);
+  int root2 = find(b);
+
+  int newSize = sets_[root1] + sets_[root2];
+  if (sets_[root1] <= sets_[root2]) {
+    sets_[root2] = root1;
+    sets_[root1] = newSize;
+  } else {
+    sets_[root1] = root2;
+    sets_[root2] = newSize;
+  }
+}
+
+int DisjointSets::size(int elem) {
+  return -sets_[find(elem)];
+}
--- mp_mazes/dsets.h
+++ mp_mazes/dsets.h
@@ -1,1 +1,25 @@
 /* Your code here! */
+
+#pragma once
+
+#include <vector>
+#include <iostream>
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+
+class DisjointSets
+{
+
+public:
+  void addelements(int num);
+  int find(int elem);
+  void setunion(int a, int b);
+  int size(int elem);
+
+private:
+  vector<int> sets_;
+
+};
